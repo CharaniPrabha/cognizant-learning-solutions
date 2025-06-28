a8036709a31251f6e98bd02209d4f02f5e863a4a
CharaniPrabha <157684304+CharaniPrabha@users.noreply.github.com>
2025-06-28 21:26:11 +0530

    Add files via upload

---
+++ Week2 Handson/Codes/Unit Testing/JUnit_Basic Testing Exercises/Exercise 4: Arrange-Act-Assert (AAA) Pattern/CalculatorTest.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    private Calculator calculator;
+
+    @Before
+    public void setUp() {
+        System.out.println("Setting up...");
+        calculator = new Calculator();  // Arrange
+    }
+
+    @After
+    public void tearDown() {
+        System.out.println("Cleaning up...");
+        calculator = null;
+    }
+
+    @Test
+    public void testAddition() {
+        // Act
+        int result = calculator.add(2, 3);
+
+        // Assert
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        // Act
+        int result = calculator.subtract(5, 2);
+
+        // Assert
+        assertEquals(3, result);
+    }
+}
